5df0c2047f370a973ff9fe73902c17e55edb4838
Josiah Dahl <josiah.dahl@gmail.com>
2018-05-30 09:55:09 -0700

    feat: Add redux-persist to save state

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
-import { store } from './store';
+import { persistor, store } from './store';
 import registerServiceWorker from './registerServiceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <PersistGate loading={null} persistor={persistor}>
+      <App/>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import localForage from 'localforage';
 import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from '../reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage: localForage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(
     thunkMiddleware,
   )
 );
+
+export const persistor = persistStore(store);
